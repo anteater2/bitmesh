6b7d34d9b5915082e8523f92ac7bbbd8a740829a
Yuchen Pang <pyc@Yuchens-MacBook-Pro.local>
2017-12-04 22:38:17 -0600

    separate dht and chord

--- test.go
+++ test.go
@@ -1,44 +1,37 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"math/rand"
-	"time"
 
 	"github.com/anteater2/bitmesh/dht"
 )
 
 func main() {
-	dht.Start()
-	test := false
-	for _, a := range flag.Args() {
-		if a == "test" {
-			test = true
-		}
+	t, err := dht.New("172.17.0.2:2001", 2001, 2000)
+	if err != nil {
+		panic(err)
 	}
-	if test {
-		time.Sleep(10 * time.Second)
-		max := 1000
+	t.Start()
 
-		for i := 0; i < max; i++ {
-			k := fmt.Sprintf("%d", i)
-			err := dht.Put(k, k)
-			if err != nil {
-				panic(err)
-			}
+	max := 1000
+	for i := 0; i < max; i++ {
+		k := fmt.Sprintf("%d", i)
+		err := t.Put(k, k)
+		if err != nil {
+			panic(err)
 		}
-		for i := 0; i < 100; i++ {
-			k := fmt.Sprintf("%d", rand.Int()%max)
-			v, err := dht.Get(k)
-			if err != nil {
-				panic(err)
-			}
-			if string(k) != v {
-				panic("unmatch")
-			}
-		}
-		fmt.Println("done")
 	}
+	for i := 0; i < 100; i++ {
+		k := fmt.Sprintf("%d", rand.Int()%max)
+		v, err := t.Get(k)
+		if err != nil {
+			panic(err)
+		}
+		if string(k) != v {
+			panic("unmatch")
+		}
+	}
+	fmt.Println("done")
 	select {}
 }
